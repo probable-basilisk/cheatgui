c8c82846cca2119c5084c89285fdaaad0cd2144d
probable-basilisk <basilisk@mtknn.com>
2020-01-31 19:02:54 -0500

    cleanup

--- data/hax/hax.lua
+++
@@ -1,2 +0,0 @@
---dofile("data/hax/ws.lua") -- WIP websocket API
-dofile("data/hax/cheatgui.lua")
--- init.lua
+++ init.lua
@@ -1,29 +1,13 @@
-function OnModPreInit()
-	-- Nothing to do but this function has to exist
-end
-
-function OnModInit()
-	-- Nothing to do but this function has to exist
-end
-
-function OnModPostInit()
-	-- Nothing to do but this function has to exist
-end
-
-function OnWorldPreUpdate()
-	-- Nothing to do but this function has to exist
-end
-
 function OnWorldPostUpdate() 
-	if _cheat_gui_main then _cheat_gui_main() end
+  if _cheat_gui_main then _cheat_gui_main() end
 end
 
 function OnPlayerSpawned( player_entity )
-	print("OnPlayerSpawned require check:")
-	if not require then
-		print("NO require.")
-	else
-		print("YES require.")
-	end
-	dofile("data/hax/cheatgui.lua")
+  print("OnPlayerSpawned require check:")
+  if not require then
+    print("NO require.")
+  else
+    print("YES require.")
+  end
+  dofile("data/hax/cheatgui.lua")
 end
